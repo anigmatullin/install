c69eacfd70e42a3d9ea8f2ef2718941e0b9e6d39
Aibulat <nigmatullin@gmail.com>
2022-04-05 14:34:17 +0500

    initial

---
+++ mysql/mysql-ubuntu.sh
@@ -0,0 +1,13 @@
+
+sudo apt install mysql-server
+
+systemctl start mysql.service
+systemctl enable mysql.service
+
+sudo mysql-secure-installation
+
+sudo mysql
+
+echo "https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-20-04"
+
+
