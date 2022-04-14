3548ef887fbff27c9f7464d1e94b743d22a05015
Aibulat <nigmatullin@gmail.com>
2022-04-15 00:37:33 +0500

    cleaner

--- ngm/mail/script.ts
+++ ngm/mail/script.ts
@@ -3,6 +3,19 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
+
+  let item = {
+      data: {
+        src_ip: "127.0.0.1",
+        src_port: 44456,
+        helo_name: "localhost",
+        sender: "sender@localhost",
+        recipients: "to@localhost"
+      }
+  };
+
+  await prisma.maillog.create(item);
+
   const logs = await prisma.maillog.findMany({});
 
   console.log(logs);
