4c2e36efe726e92ddd1722e0899b711baa3c2734
Aibulat <nigmatullin@gmail.com>
2022-04-20 21:22:56 +0500

    headers; attachment info for mails

---
+++ ngm/mail/prisma/migrations/20220420162110_headers_attaches/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE `Header` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `mail_id` INTEGER NOT NULL DEFAULT 0,
+    `name` VARCHAR(191) NOT NULL DEFAULT '',
+    `value` VARCHAR(191) NOT NULL DEFAULT '',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Attach` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `mail_id` INTEGER NOT NULL DEFAULT 0,
+    `name` VARCHAR(191) NOT NULL DEFAULT '',
+    `filetype` VARCHAR(191) NOT NULL DEFAULT '',
+    `hash` VARCHAR(191) NOT NULL DEFAULT '',
+    `size` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
