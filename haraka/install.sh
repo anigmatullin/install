a52aa82e2cc8f33e42313c642a43336c7e790f5f
Aibulat <nigmatullin@gmail.com>
2022-07-05 01:03:03 +0500

    Haraka installer

---
+++ haraka/install.sh
@@ -0,0 +1,19 @@
+
+sudo npm install -g Haraka
+
+haraka -v
+
+sudo systemctl status postfix
+sudo systemctl stop postfix
+sudo systemctl disable postfix
+
+ss -lstn | grep 25
+
+sudo apt install swaks
+
+swaks -v
+
+
+sudo npm install -g pm2
+
+pm2
